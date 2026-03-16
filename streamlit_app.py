b9bb86555f954d569989ec8b6c3697cd835bf7b4
pablo1przepyszny <p.klinkoszz@gmail.com>
2026-03-16 17:21:57 +0100

    Update streamlit_app.py
    +tabela

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,6 +47,7 @@ if ingredients_list:
   
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 if smoothiefroot_response.status_code == 200:
-    st.text(smoothiefroot_response.json())
+    #st.text(smoothiefroot_response.json())
+    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 else:
     st.error(f"Error: {smoothiefroot_response.status_code}")
