7e81710c1de8f97d9b35749fe3a65abe486ef8a8
pablo1przepyszny <p.klinkoszz@gmail.com>
2026-03-16 18:57:53 +0100

    Update streamlit_app.py
    moved two lines into the fruit chosen block

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -45,9 +47,8 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
 
   
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+
 if smoothiefroot_response.status_code == 200:
-    #st.text(smoothiefroot_response.json())
-    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+  
 else:
     st.error(f"Error: {smoothiefroot_response.status_code}")
